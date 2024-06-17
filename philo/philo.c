e3c1e29b45c6b1817fe40e155b21640df3a9046b
Jordan Gavairon <jgavairo@z1r3p3.42lyon.fr>
2024-06-17 16:39:35 +0200

    first push

--- philo/philo.c
+++ philo/philo.c
@@ -6,48 +6,142 @@
 /*   By: jgavairo <jgavairo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 16:20:14 by jgavairo          #+#    #+#             */
-/*   Updated: 2024/05/30 17:25:58 by jgavairo         ###   ########.fr       */
+/*   Updated: 2024/06/17 16:37:33 by jgavairo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-init_philosophers()
+long get_timestamp() 
 {
-	
+    struct timeval tv;
+
+    gettimeofday(&tv, NULL);
+    return (tv.tv_sec * 1000 + tv.tv_usec / 1000);
 }
 
-start_simulation()
+int init_philosophers(t_data *data, t_philosopher **philo)
 {
+	int	i;
 	
+	i = 0;
+	(*philo) = malloc(sizeof(t_philosopher) * data->number_of_philosophers);
+	if (!(*philo))
+		return (-1);
+	while (i < data->number_of_philosophers)
+	{
+		(*philo)[i].id = i + 1;
+		(*philo)[i].data = data;
+		(*philo)[i].meals_eaten = 0;
+		(*philo)[i].left_fork_bool = false;
+		(*philo)[i].right_fork_bool = (*philo)[(i + 1) % data->number_of_philosophers].left_fork_bool;
+		(*philo)[i].last_meal= get_timestamp();
+		(*philo)[i].left_fork = malloc(sizeof(pthread_mutex_t));
+		pthread_mutex_init(philo[i]->left_fork, NULL);
+		(*philo)[i].right_fork = ((*philo)[(i + 1) % data->number_of_philosophers].left_fork);
+		if (pthread_create(&((*philo)[i].thread), NULL, philo_routine, &((*philo)[i])) != 0)
+			return (-1);
+		printf("lolllllll\n");
+		i++;
+	}
+	data->start = 1;
+	return (0);
+}
+
+void philo_eat(t_philosopher *philo)
+{
+	while (1)
+	{
+		pthread_mutex_lock(philo->left_fork);
+		if (philo->left_fork_bool == false)
+		{
+			philo->left_fork_bool = true;
+			printf ("Le philosopher %d a pris la fourchette de gauche\n", philo->id);
+			pthread_mutex_unlock(philo->left_fork);
+			break ;
+		}
+		else
+		{
+			pthread_mutex_unlock(philo->left_fork);
+			usleep(20);
+		}
+	}
+	while (1)
+	{
+		pthread_mutex_lock(philo->right_fork);
+		if (philo->right_fork_bool == false)
+		{
+			philo->right_fork_bool = true;
+			printf ("Le philosopher %d a pris la fourchette de droite\n", philo->id);
+			pthread_mutex_unlock(philo->right_fork);
+			printf("Le philo %d mange\n", philo->id);
+			philo->last_meal = get_timestamp();
+			philo->meals_eaten++;
+			usleep(philo->data->time_to_sleep * 1000);
+			pthread_mutex_lock(philo->left_fork);
+			philo->left_fork_bool = false;
+			pthread_mutex_unlock(philo->left_fork);
+			pthread_mutex_lock(philo->right_fork);
+			philo->right_fork_bool = false;
+			pthread_mutex_unlock(philo->right_fork);
+			break ;
+		}
+		else
+		{
+			pthread_mutex_unlock(philo->right_fork);
+			usleep(20);
+		}
+	}
+}
+
+void	philo_sleep(t_philosopher *philo)
+{	
+	printf("le philo %d dort...\n", philo->id);
+	usleep(philo->data->time_to_sleep);
+	printf("le philo %d pense...\n", philo->id);
+}
+
+void	*philo_routine(void *arg)
+{
+	t_philosopher *philo = (t_philosopher *)arg;
+	
+	while (1)
+	{
+		if (philo->data->start == 1)
+			break ;
+	}
+	while (1)
+	{
+		printf("Le philosopher %d essaie de manger...\n", philo->id);
+		philo_eat(philo);
+		printf("Le philosopher %d dors...\n", philo->id);
+		philo_sleep(philo);
+	}
+	return (NULL);
 }
 
 int	main(int argc, char **argv)
 {
-	t_params 	params;
+	t_data 	data;
+	t_philosopher *philo;
 	int			i;
 	
 	i = 0;
 	if (argc < 5 || argc > 6)
 		return(printf("error: invalid argument : (number_of_philosophers time_to_die time_to_eat time_to_sleep [number_of_meals])\n"), -1);
 	//--------------------stock entrees dans la structure des parametres
-	params.number_of_philosophers = atoi(argv[1]);
-	params.time_to_die = atol(argv[2]);
-	params.time_to_eat = atol(argv[3]);
-	params.time_to_sleep = atol(argv[4]);
+	data.start = 0;
+	data.number_of_philosophers = atoi(argv[1]);
+	data.time_to_die = atol(argv[2]);
+	data.time_to_eat = atol(argv[3]);
+	data.time_to_sleep = atol(argv[4]);
 	if (argv[5])
-		params.number_of_meals = atol(argv[5]);
+		data.number_of_meals = atol(argv[5]);
 	else
-		params.number_of_meals = -1;
+		data.number_of_meals = -1;
 	//-------------------
-	params.forks = malloc(sizeof(pthread_mutex_t) * params.number_of_philosophers);
-	if (!params.forks)
+	init_philosophers(&data, &philo);
+	if (!philo)
 		return (-1);
-	//-------------------Initialisation des forks
-	while (i < params.number_of_philosophers)
-	{
-		pthread_mutex_init(&params.forks[i], NULL);
-		i++;
-	}
 	
 }
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: jgavairo <jgavairo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 16:20:12 by jgavairo          #+#    #+#             */
-/*   Updated: 2024/05/30 16:26:26 by jgavairo         ###   ########.fr       */
+/*   Updated: 2024/06/17 16:33:47 by jgavairo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,29 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <sys/time.h>
+#include <stdbool.h>
 
-typedef struct s_philosopher {
-    int id;
-    pthread_t thread;
-    pthread_mutex_t *left_fork;
-    pthread_mutex_t *right_fork;
-    long last_meal;
-    int meals_eaten;
-    t_params *params;
-} t_philosopher;
+typedef struct s_data
+{
+    int					number_of_philosophers;
+    long				time_to_die;
+    long				time_to_eat;
+    long				time_to_sleep;
+    int					number_of_meals;
+	int					start;
+}						t_data;
 
-typedef struct s_params {
-    int number_of_philosophers;
-    long time_to_die;
-    long time_to_eat;
-    long time_to_sleep;
-    int number_of_meals;
-    pthread_mutex_t *forks;
-    t_philosopher *philosophers;
-} t_params;
+typedef struct s_philosopher
+{
+    int					id;
+    pthread_t			thread;
+    pthread_mutex_t		*left_fork;
+    bool				left_fork_bool;
+    pthread_mutex_t		*right_fork;
+	bool				right_fork_bool;
+    long				last_meal;
+    int					meals_eaten;
+    t_data				*data;
+}						t_philosopher;
 
+void *philo_routine(void *arg);
