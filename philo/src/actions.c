4c960ea2345866bbe746600a52911118e5ddf11a
Ja2s <jordan.gavairon@gmail.com>
2024-06-28 15:05:50 +0200

    i cant more but datablock

--- philo/src/actions.c
+++ philo/src/actions.c
@@ -6,52 +6,25 @@
 /*   By: jgavairo <jgavairo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 15:24:37 by jgavairo          #+#    #+#             */
-/*   Updated: 2024/06/27 15:05:16 by jgavairo         ###   ########.fr       */
+/*   Updated: 2024/06/28 15:02:48 by jgavairo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philo.h"
 
-int	take_left_fork(t_philosopher *philo)
-{
-	if (death_checker(philo) == -1)
-		return (-1);
-	pthread_mutex_lock(philo->left_fork);
-	if (philo->left_fork_bool == false)
-	{
-		philo->left_fork_bool = true;
-		pthread_mutex_unlock(philo->left_fork);
-		if (write_status(philo, "has taken a fork") == -1)
-				return (-1);
-		return (0);
-	}
-	else
-	{
-		pthread_mutex_unlock(philo->left_fork);
-		return (-1);
-	}
-	//usleep(1000); //-------------------
-}
-
 int	eating(t_philosopher *philo)
 {
 	long	time;
 
-	if (death_checker(philo) == -1)
+	if (write_status(philo, "is eating") == -1)
 		return (-1);
-	pthread_mutex_lock(&philo->data->eat_time_mut);
 	time = (philo->data->time_to_eat);
-	pthread_mutex_unlock(&philo->data->eat_time_mut);
 	if (death_checker(philo) == -1)
 		return (-1);
-	//pthread_mutex_lock(&philo->data->meals);
-	pthread_mutex_lock(&philo->data->eat_time_mut);
+	pthread_mutex_lock(&philo->data->meals);
 	philo->meals_eaten++;
 	philo->last_meal = get_timestamp();
-	pthread_mutex_unlock(&philo->data->eat_time_mut);
-	//pthread_mutex_unlock(&philo->data->meals);
-	if (death_checker(philo) == -1)
-		return (-1);
+	pthread_mutex_unlock(&philo->data->meals);
 	ft_usleep_check(philo, time);
 	if (death_checker(philo) == -1)
 		return (-1);
@@ -72,37 +45,43 @@ int	philo_eat(t_philosopher *philo)
 	{
 		if (death_checker(philo) == -1)
 			return (-1);
-
-		if (take_left_fork(philo) != 0)
+		pthread_mutex_lock(philo->left_fork);
+		if (philo->left_fork_bool == true)
 		{
-			if (death_checker(philo) == -1)
-				return (-1);
-			usleep(5);
-			continue;
+			pthread_mutex_unlock(philo->left_fork);
+			usleep(150);
+			continue ;
 		}
-
-		while (1)
-		{	
-			if (death_checker(philo) == -1)
+		else
+		{
+			philo->left_fork_bool = true;
+			pthread_mutex_unlock(philo->left_fork);
+			if (write_status(philo, "has take a fork") == -1)
 				return (-1);
-			pthread_mutex_lock(philo->right_fork);
-			if (*(philo->right_fork_bool) == true)
+			while (1)
 			{
-				pthread_mutex_unlock(philo->right_fork);
-				usleep(5);
-				continue;
+				if (death_checker(philo) == -1)
+					return (-1);
+				pthread_mutex_lock(philo->right_fork);
+				if (*philo->right_fork_bool == true)
+				{
+					pthread_mutex_unlock(philo->right_fork);
+					usleep(150);
+					continue ;
+				}
+				else
+				{
+					*philo->right_fork_bool = true;
+					pthread_mutex_unlock(philo->right_fork);
+					if (write_status(philo, "has take a fork") == -1)
+						return (-1);
+					eating(philo);
+					return (0);
+				}
+				usleep (1000);
 			}
-			*(philo->right_fork_bool) = true;
-			pthread_mutex_unlock(philo->right_fork);
-			if (write_status(philo, "has taken a fork") == -1)
-				return (-1);
-			if (write_status(philo, "is eating") == -1)
-				return (-1);
-			if (eating(philo) == -1)
-				return (-1);
-			return (0);
 		}
-		//usleep(1000); //-------------------
+		usleep (1000);
 	}
 	return (0);
 }
@@ -115,10 +94,6 @@ int	philo_sleep(t_philosopher *philo)
 		return (-1);
 	ft_usleep_check(philo, philo->data->time_to_sleep);
 	//usleep(philo->data->time_to_sleep * 1000);
-	if (death_checker(philo) == -1)
-		return (-1);
-	if (write_status(philo, "is thinking") == -1)
-		return (-1);
 	// ft_usleep_check(philo, 1);
 	//usleep(philo->data->time_to_die - (get_timestamp() - philo->last_meal) - 1000);
 	return (0);
@@ -128,21 +103,26 @@ void	*philo_routine(void *arg)
 {
 	t_philosopher	*philo;
 
+	long time_eat;
 	philo = (t_philosopher *)arg;
+	
+	time_eat = (philo->data->time_to_eat / 2) * 1000;
 	while (1)
 	{
 		pthread_mutex_lock(&philo->data->start_mut);
 		if (philo->data->start == 1)
 		{
 			pthread_mutex_unlock(&philo->data->start_mut);
+			pthread_mutex_lock(&philo->data->meals);
 			philo->last_meal = get_timestamp();
+			pthread_mutex_unlock(&philo->data->meals);
 			break ;
 		}
 		pthread_mutex_unlock(&philo->data->start_mut);
-		//ft_usleep(1);
+		usleep (1000);
 	}
 	if (philo->id % 2 == 0)
-		usleep(philo->data->time_to_eat / 4 * 1000);
+		usleep(time_eat);
 	while (1)
 	{
 		if (philo_eat(philo) == -1)
@@ -153,7 +133,8 @@ void	*philo_routine(void *arg)
 			return (NULL);
 		if (death_checker(philo) == -1)
 			return (NULL);
-		//usleep(1000); //-------------------
+		usleep(150);
+		usleep (1000);
 	}
 	return (NULL);
 }
--- philo/src/utils.c
+++ philo/src/utils.c
@@ -6,7 +6,7 @@
 /*   By: jgavairo <jgavairo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 16:20:08 by jgavairo          #+#    #+#             */
-/*   Updated: 2024/06/27 14:29:08 by jgavairo         ###   ########.fr       */
+/*   Updated: 2024/06/28 15:01:58 by jgavairo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,8 +33,8 @@ void	ft_usleep(int time)
     while ((get_timestamp() - start_time) < time)
     {
         time_to_wait = ((time - (get_timestamp() - start_time)) * 1000) / 2;
-        if (time_to_wait > 50)
-            usleep(50);
+        if (time_to_wait > 150)
+            usleep(150);
         else
             usleep(time_to_wait);
     }
@@ -51,8 +51,8 @@ void	ft_usleep_check(t_philosopher *philo, int time)
 		if (death_checker(philo) == -1)
 			return ;
         time_to_wait = ((time - (get_timestamp() - start_time)) * 1000) / 2;
-        if (time_to_wait > 50)
-            usleep(50);
+        if (time_to_wait > 150)
+            usleep(150);
         else
             usleep(time_to_wait);
     }
@@ -78,24 +78,22 @@ long	get_timestamp(void)
 
 int	check_nb_meals(t_data *data, t_philosopher *philo)
 {
-	//pthread_mutex_lock(&data->meals);
-	pthread_mutex_lock(&philo->data->eat_time_mut);
+	pthread_mutex_lock(&philo->data->meals);
 	if (data->number_of_meals != -1 && \
 	philo->meals_eaten >= data->number_of_meals)
 		data->max_meal = 1;
 	else
 		data->max_meal = 0;
-	pthread_mutex_unlock(&philo->data->eat_time_mut);
-	//pthread_mutex_unlock(&data->meals);
+	pthread_mutex_unlock(&philo->data->meals);
 	return (0);
 }
 
 int	check_death(t_philosopher *philo, t_data *data)
 {
-	pthread_mutex_lock(&data->time_mut);
+	pthread_mutex_lock(&data->meals);
 	if (get_timestamp() - philo->last_meal >= philo->data->time_to_die)
 	{
-		pthread_mutex_unlock(&data->time_mut);
+		pthread_mutex_unlock(&data->meals);
 		pthread_mutex_lock(&data->stop_mut);
 		data->stop = 1;
 		pthread_mutex_unlock(&data->stop_mut);
@@ -104,6 +102,6 @@ int	check_death(t_philosopher *philo, t_data *data)
 		pthread_mutex_unlock(&data->print);
 		return (-1);
 	}
-	pthread_mutex_unlock(&data->time_mut);
+	pthread_mutex_unlock(&data->meals);
 	return (0);
 }
